4889b93c130ec3b98df2f50e104b4d9472f62a24
Sudha Poojari <sudha.poojari@hyperisland.se>
2021-10-29 15:24:16 +0200

    Added website landing page
    - Added HTML, CSS, JS and related image files

---
+++ quiz.js
@@ -0,0 +1,11 @@
+let imgBx = document.querySelectorAll('.imgBx');
+		imgBx.forEach(popup => popup.addEventListener('click', () =>{
+			popup.classList.toggle('active')
+		}))
+
+		function toggleMenu(){
+	      var menuToggle = document.querySelector('.toggle');
+	      var navigation = document.querySelector('.navigation');
+	      menuToggle.classList.toggle('active');
+	      navigation.classList.toggle('active');
+	    }
