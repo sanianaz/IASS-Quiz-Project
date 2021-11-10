216b86153c269152dd7dbef34a91d48aeec57e80
Ian W <90833604+IanWallenberg@users.noreply.github.com>
2021-11-10 11:31:42 +0100

    Update end.js

--- scripts/end.js
+++ scripts/end.js
@@ -38,7 +38,7 @@ saveHighScore = e => {
 
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("#");
+    window.location.assign("/index.html");
     //console.log(highScores);
 };
 
